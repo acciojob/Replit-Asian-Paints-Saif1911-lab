f423fabbc4fb6c427648a079ac5e59fe4ac0b439
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-06 11:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-//your JS code here. If required.
 const changeBtn = document.getElementById("change_button");
 
 changeBtn.addEventListener('click', () => {
-	const blockId = document.getElementById('block_id').value
-	const color = document.getElementById('colour_id').value
-	const blockEle = document.getElementById(blockId);
-	blockEle.style.backgroundColor = color;
-})
+  const blockId = document.getElementById('block_id').value;
+  const color = document.getElementById('colour_id').value;
+  const blockEle = document.getElementById(blockId);
 
-const resetBtn = document.getElementById('reset');
+  if (blockEle) {
+    blockEle.style.backgroundColor = color;
+  }
+});
+
+const resetBtn = document.getElementById('reset_button');
 
 resetBtn.addEventListener('click', () => {
-	const blocks = document.querySelectorAll('.grid-item');
-	blocks.forEach(b => b.style.backgroundColor = 'transparent')
-})
+  const blocks = document.querySelectorAll('.grid-item');
+  blocks.forEach(b => b.style.backgroundColor = 'transparent');
+});
